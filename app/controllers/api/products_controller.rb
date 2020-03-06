c9fc049e7b8d19b637254436b26aeb81aaf9beb0
Nikki <nikmck22@gmail.com>
2020-03-05 22:21:20 -0500

    add create action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -7,7 +7,18 @@ class Api::ProductsController < ApplicationController
 
   def show
     the_id = params[:id]
-    @products = Product.find_by(id: the_id)
+    @product = Product.find_by(id: the_id)
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:input_name],
+      price: params[:input_price],
+      image_url: params[:input_image_url],
+      description: params[:input_description]
+    )
+    @product.save
     render "show.json.jb"
   end
 end

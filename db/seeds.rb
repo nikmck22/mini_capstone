8363784159879f4f6cca7b9689e16964430117ea
Nikki <nikmck22@gmail.com>
2020-03-10 22:29:27 -0400

    seeds migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-fake_name = Faker::Book.title
-fake_description = Faker::Book.genre + ", " + Faker::Book.author
-
-product = Product.create({name: fake_name, price: 2, image_url: "text", description: fake_description})
-
-
-fake_name = Faker::Book.title
-fake_description = Faker::Book.genre + ", " + Faker::Book.author
-
-product = Product.create({name: fake_name, price: 2, image_url: "text", description: fake_description})
-              
+Product.create!([
+  {name: "Postern of Fate", price: "2.0", image_url: "https://thumbs-prod.si-cdn.com/c0VP9nb5bbooVSqQEjHk3q9S35c=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg", description: "Fable,Wonda Mohr"},
+  {name: "The Golden Bowl", price: "2.0", image_url: "https://www.incimages.com/uploaded_files/image/970x450/getty_883231284_200013331818843182490_335833.jpg", description: "Essay, Mr. Tyisha Huel"},
+  {name: "All Passion Spent", price: "2.0", image_url: "https://odcspress.org/wp-content/uploads/2017/01/Stack-Books-Copy.jpg", description: "Speech, Evangelina Halvorson"},
+  {name: "desk", price: "300.0", image_url: "https://www.ikea.com/us/en/images/products/arkelstorp-desk__0735967_PE740301_S5.JPG", description: "sit and work, wood, fits in an office"},
+  {name: "book", price: "25.0", image_url: "https://www.pe.com/wp-content/uploads/2019/06/636337794-1.jpg?w=525", description: "how to code"},
+  {name: "The Moving Finger", price: "2.0", image_url: "text", description: "Western, Bruna Wunsch"},
+  {name: "Vanity Fair", price: "2.0", image_url: "text", description: "Fantasy, Valene Hane"},
+  {name: "Mother Night", price: "2.0", image_url: "text", description: "Historical fiction, Damaris Breitenberg"},
+  {name: "An Acceptable Time", price: "2.0", image_url: "text", description: "Horror, Mrs. Randal Kuhn"},
+  {name: "Bury My Heart at Wounded Knee", price: "2.0", image_url: "text", description: "Western, Mike Emmerich"},
+  {name: "Paths of Glory", price: "2.0", image_url: "text", description: "Western, Kiesha McGlynn"},
+  {name: "Lilies of the Field", price: "2.0", image_url: "text", description: "Mythopoeia, Sidney Zulauf"},
+  {name: "Quo Vadis", price: "2.0", image_url: "text", description: "Folklore, Juanita Howe"},
+  {name: "Blood's a Rover", price: "2.0", image_url: "text", description: "Horror, Ms. Sharilyn Lehner"},
+  {name: "The Needle's Eye", price: "2.0", image_url: "text", description: "Biography/Autobiography, Margurite Cronin"}
+])

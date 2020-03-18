f036dd496e8c09ec265262aed937df91380201c7
Nikki <nikmck22@gmail.com>
2020-03-17 21:49:53 -0400

    create Orders model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  has_many :users
+  
   validates :name, presence: true
   validates :name, length: { maximum: 40 }
   validates :price, presence: true
